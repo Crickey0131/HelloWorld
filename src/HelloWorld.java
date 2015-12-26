c09d03d943ccd6d817b4dbbeed0d858fdbb886fb
Crickey0131 <Xi@Xis-MacBook-Pro-2.local>
2015-12-25 17:39:47 -0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world!");
+
+		ArrayList a = new ArrayList();
+		LinkedList s = new LinkedList();
+	}
+
+}
